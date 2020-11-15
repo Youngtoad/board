06b69cdf828cc647352e9fcbf695f4697e67fd78
김영진 <youngjin033@ajou.ac.kr>
2020-11-15 21:45:07 +0900

    livereload 설정

--- src/main/java/com/youngtoad/board/controller/BoardController.java
+++ src/main/java/com/youngtoad/board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.youngtoad.board.Controller;
+package com.youngtoad.board.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
