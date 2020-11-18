868c54eb5ded0f54048f77583c5e0342b638f8c2
김영진 <youngjin033@ajou.ac.kr>
2020-11-18 15:52:50 +0900

    update 및 delete기능 추가

--- src/main/java/com/youngtoad/board/controller/BoardController.java
+++ src/main/java/com/youngtoad/board/controller/BoardController.java
@@ -4,9 +4,10 @@ import com.youngtoad.board.dto.BoardDto;
 import com.youngtoad.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -14,8 +15,47 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/")
-    public String list(){
+    public String list(Model model) {
+        List<BoardDto> boardList = boardService.getBoardList();
+
+        model.addAttribute("boardList", boardList);
         return "board/list.html";
+
+    }
+
+    //게시글 상세조회
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    //게시글 수정 페이지
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+
+    //게시글 수정
+   @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    //게시글 삭제제
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no){
+        boardService.deletePost(no);
+
+        return "redirect:/";
     }
 
     @GetMapping("/post")
--- src/main/java/com/youngtoad/board/service/BoardService.java
+++ src/main/java/com/youngtoad/board/service/BoardService.java
@@ -1,11 +1,15 @@
 package com.youngtoad.board.service;
 
+import com.youngtoad.board.domain.entity.BoardEntity;
 import com.youngtoad.board.dto.BoardDto;
 import com.youngtoad.board.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -13,7 +17,47 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
+    public List<BoardDto> getBoardList(){
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(BoardEntity boardEntity : boardEntities){
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
+    }
+
+    @Transactional
+    public BoardDto getPost(Long id){
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDto;
+    }
+
+    @Transactional
     public Long savePost(BoardDto boardDto){
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void deletePost(Long id){
+        boardRepository.deleteById(id);
     }
 }
